b56b1f89a2c09d0d2d7b8d324b8d5104f3c53093
Vivek Kumar Singh <64981263+singhvivekkumar@users.noreply.github.com>
2023-07-03 23:06:23 +0530

    new update of watch page

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useSearchParams } from "react-router-dom";
@@ -8,15 +8,24 @@ import RelatedVideos from "./RelatedVideos";
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
-  console.log(searchParams.get("v"));
+  // console.log(searchParams.get("v"));
+  const [relatedVideos, setRelatedVideos] = useState([]);
 
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(closeMenu());
+    getRelatedVideos();
   }, []);
 
+  const getRelatedVideos = async (id) => { 
+    const response = await fetch();
+    const jsonData = await response.json();
+    console.log(jsonData)
+    // setRelatedVideos(jsonData)
+   }
+
   return (
-    <div className="flex w-full">
+    <div className="flex justify-center flex-row w-full">
       {/* section 1st */}
       <div>
         <div className=" px-10 m-4 ">
@@ -29,6 +38,9 @@ const WatchPage = () => {
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowFullScreen
           ></iframe>
+          <div className="text-white font-bold text-sm md:text-xl mt-4 line-clamp-2">
+            {searchParams?.title}
+          </div>
         </div>
 
         {/* nested comment below */}
@@ -38,8 +50,19 @@ const WatchPage = () => {
       </div>
       {/* section 2nd */}
       <div className=" m-1 mr-5 w-full">
-        <LiveChat/>
-        <RelatedVideos/>
+        <LiveChat />
+        <RelatedVideos />
+        <div className="flex flex-col py-6 px-4 overflow-y-auto lg:w-[350px] xl:w-[400px]">
+                    {relatedVideos?.contents?.map((item, index) => {
+                        if (item?.type !== "video") return false;
+                        return (
+                            <RelatedVideos
+                                key={index}
+                                video={item?.video}
+                            />
+                        );
+                    })}
+                </div>
       </div>
     </div>
   );
