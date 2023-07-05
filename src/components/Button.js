e729b5912a3e2a4e15f4a98dad7beed8f1db02a6
Vivek Kumar Singh <64981263+singhvivekkumar@users.noreply.github.com>
2023-07-05 22:46:58 +0530

    new button is added

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({name}) => {
   return (
 	<div>
-		<button className=' p-1 px-2 m-1 mx-1 text-sm bg-gray-200 rounded-md'>{name}</button>
+		<button className=' p-1 px-2 m-1 mx-1 text-sm  bg-gray-200 rounded-md'>{name}</button>
 	</div>
   )
 }
